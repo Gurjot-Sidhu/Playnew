3433b89193848cebc65ec57fc717266846f3679f
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-03-26 09:49:05 -0400

    working

---
+++ playlister-frontend/index.js
@@ -0,0 +1,159 @@
+let allPlaylistData;
+function start(){
+fetch("http://localhost:3000/playlists")
+  .then((response) => response.json())
+  .then(renderAllPlaylist)
+}
+start()
+playCont = document.querySelector("#playlist-list")
+
+function renderAllPlaylist(allplaylists){
+    allplaylists.forEach(renderOnePlaylist)
+}
+
+function renderOnePlaylist(playlist){
+    outerli = document.createElement("li")
+    outerli.id = playlist.id
+    outerli.textContent = playlist.name
+
+    newsongbtn = document.createElement("BUTTON")
+    newsongbtn.id = "songbtn"
+    newsongbtn.className = playlist.id
+    newsongbtn.textContent = "Add a song to this playlist"
+    newsongbtn.value = "addsong"
+
+    modal = document.createElement("div")
+    modal.innerHTML =`
+    <form id="newsongform" style = "display: none;">
+      <h3>Add new Song</h3>
+      <label for="title">Title</label>
+      <input type="text" name="title">
+      <label for="artist">Artist</label>
+      <input type="text" name="artist">
+      <label for="genre">Genre</label>
+      <input type="text" name="genre">
+      <label for="cover">Cover</label>
+      <input type="text" name="cover">
+      <input type="hidden" name="playlistid" value="${playlist.id}">
+      <input type="submit" value="Add Song">
+    </form>`
+    
+
+    outerli.append(newsongbtn)
+    outerli.append(modal)
+    playCard.append(outerli)
+    
+    renderAllSongs(playlist)
+}
+
+playCard = document.querySelector("#playlist-card")
+
+
+function renderAllSongs(playlist){
+    const songs = [...playlist.songs]
+    songextractor(songs)
+    function songextractor(songs){
+        for(var i = 0; i < songs.length;i++){
+            songpara = document.createElement("p")
+            songpara.innerHTML = `Song: ${songs[i].title} Artist: ${songs[i].artist} Genre: ${songs[i].genre}`
+            songimg = document.createElement("img")
+            songimg.src = songs[i].cover
+            songpara.append(songimg)
+            playCard.append(songpara)
+        }
+    }
+}
+
+function renderOneSong(song){
+    const playid = song.playlist.id
+    const newSongLi = document.getElementById(`${playid}`)
+    songpara = document.createElement("p")
+    songpara.innerHTML = `${song.title} artist: ${song.artist}`
+    songimg = document.createElement("img")
+    songimg.src = song.cover
+    songpara.append(songimg)
+    newSongLi.append(songpara)
+}
+
+
+
+newForm = document.querySelector("#create-playlist")
+newForm.addEventListener("submit",handleNewSubmit)
+
+function handleNewSubmit(event){
+    event.preventDefault()
+
+    // playlist = {
+    //     name:newplay
+    // }
+    const newplay = newForm["name"].value
+
+    fetch("http://localhost:3000/playlists", {
+      method: `POST`,
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: newplay})
+    }).then(res => res.json())
+    .then(renderOnePlaylist)
+
+    newForm.reset()
+}
+
+
+playCont.addEventListener("click",e =>{
+    if(e.target.value === "Add Song"){
+        event.preventDefault()
+        const card = e.target.closest("li")
+        newplaylistid = card.id
+        newplaylistname = card.textContent
+        const songForm = e.target.closest("form")
+        
+        const newtitle = songForm["title"].value
+        const newartist = songForm["artist"].value
+        const newgenre = songForm["genre"].value
+        const newcover = songForm["cover"].value
+
+        fetch("http://localhost:3000/songs", {
+        method: `POST`,
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            },
+        body: JSON.stringify({
+            title: newtitle,
+            artist: newartist,
+            genre: newgenre,
+            cover: newcover,
+            playlist_id: newplaylistid
+            })
+        }).then(res => res.json())
+        .then(renderOneSong)
+
+        songForm.reset()
+        
+     }
+})
+
+playCont.addEventListener("click",e =>{
+    const closeli = e.target.closest("li")
+        
+        const closediv = closeli.querySelector("div")
+        let closeform = closediv.querySelector("form")
+    if(e.target.value === "addsong" && closeform.style.display === "block"){
+        event.preventDefault()
+        closeform.style.display = "none"
+    }else if (e.target.value === "addsong" && closeform.style.display !== "block"){
+        closeform.style.display = "block"
+    }else{
+
+    }
+})
+
+
+//      CSS GOALS
+// Center and align things
+//Capitalize letters and forms text-transform: capitalize;
+//Organize playlists into cards with styling
